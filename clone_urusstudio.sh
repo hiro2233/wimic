43f31316093100a42cb5e15fbc2dab80935572c7
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2020-07-20 07:58:46 -0400

    clone: fixed cloning urusstudio by default command way.

--- clone_urusstudio.sh
+++ clone_urusstudio.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
 if [ ! -e "UrusStudio" ] ; then
-	git clone https://github.com/UrusTeam/UrusStudio.git &>/dev/null || true
+	git clone https://github.com/UrusTeam/UrusStudio.git
 	cd UrusStudio
 	git submodule init && git submodule update modules/wxWidgets
 	cd ..
 fi
+
+echo "Clone done"
 
 exit 0
