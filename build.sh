b26b6766658697763f6592ce604fdb3272955aca
Hiroshi Takey F <htakey@gmail.com>
2020-07-20 12:39:05 -0400

    build: added submodule sync update and pushd.

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 PATH=/system/urus/bin:$PATH
 export PATH
 
+git submodule sync
+git submodule update --init --recursive
+
 NO_URUSSTUDIO_MAKE_CMD=$NO_URUSSTUDIO_MAKE_CMD
 
 if [ "$NO_URUSSTUDIO_MAKE_CMD" = "no" ] ; then
@@ -11,7 +14,10 @@ else
 	export NO_URUSSTUDIO_MAKE_CMD="yes"
 fi
 
+PUSHD=$(pwd)
 cd UrusStudio && ./build-ci.sh
-cd UrusStudio/buildustd/src/plugins/contrib/wxContribItems/wxled/ && make -j2 && make install
+cd $PUSHD
+cd ./UrusStudio/buildustd/src/plugins/contrib/wxContribItems/wxled && make -j2 && make install
+cd $PUSHD
 
 exit 0
