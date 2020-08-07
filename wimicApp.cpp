fb7659229d4d80750253e3a5a726c3a645276f9a
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2020-08-07 05:21:56 -0400

    app: added implement console and args opts.

--- wimicApp.cpp
+++ wimicApp.cpp
@@ -1,26 +1,121 @@
 
 #include "wimicApp.h"
 
+#include "typedef_ext.h"
+#include <getopt.h>
+#include <signal.h>
+
+wm_system_status_t wmsystem_status;
+
 //(*AppHeaders
 #include "wimicMain.h"
 #include <wx/image.h>
 //*)
 
-IMPLEMENT_APP(wimicApp);
+IMPLEMENT_APP_NO_MAIN(wimicApp);
+
+const char *message_logo[] = {"\nWiMic, remote wireless microphone server & client.\n"
+                              "Copyright (c) 2020 Hiroshi Takey F. <htakey@gmail.com>\n"
+                              "Licensed under GPLv3 License.\n"
+                              "This program has absolutely no warranty.\n\n"};
+
+const char *message_usage[] = {"Usage: wimic [options]\n\n"
+                               "Options:\n"
+                               "-h, --help          Show this help.\n"
+                               "-a, --autostart     autostart mode.\n"
+                               "-s, --silent        disables the GUI\n"};
+
+void wimic_help() {
+	printf(message_logo[0]);
+	printf(message_usage[0]);
+	//exit(1);
+}
+
+int wimic_main(int argc, char **argv)
+{
+    wmsystem_status.silent_mode = false;
+    wmsystem_status.autostart_mode = false;
+    wmsystem_status.help_opt = false;
+
+	int next_option;
+	const char* const short_options = "has";
+	const struct option long_options[] =
+	{
+		{ "help", no_argument, NULL, 'h' },
+		{ "autostart", no_argument, NULL, 'a' },
+		{ "silent", no_argument, NULL, 's' },
+		{ NULL, 0, NULL, 0 }
+	};
+
+	while(1) {
+		// obtain a option
+		next_option = getopt_long(argc, argv, short_options, long_options, NULL);
+
+		if(next_option == -1)
+			break;  // no more options
+
+		switch(next_option) {
+
+		case 'h':      // -h or --help
+		    wmsystem_status.help_opt = true;
+			wimic_help();
+			break;
+
+		case 'a':      // -a or --autostart
+			wmsystem_status.autostart_mode = true;
+			break;
+
+		case 's':      // -s or --silent
+			wmsystem_status.silent_mode = true;
+			break;
+
+		case '?':      // Invalid option
+			wimic_help();
+
+		case -1:      // No more options
+			break;
+
+		default:      // shouldn't happen :-)
+			return(1);
+		}
+	}
+
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    wimic_main(argc, argv);
+    return wxEntry(argc, argv);
+}
 
 bool wimicApp::OnInit()
 {
+    if (wmsystem_status.help_opt) {
+#ifdef __WXMSW__
+        wxMessageBox(wxString::FromAscii(message_logo[0]) + wxString::FromAscii(message_usage[0]));
+#endif // __WXMSW__
+        exit(1);
+    }
+
     //(*AppInitialize
     bool wxsOK = true;
     wxInitAllImageHandlers();
     if ( wxsOK )
     {
-    	wimicDialog Dlg(0);
-    	SetTopWindow(&Dlg);
-    	Dlg.ShowModal();
-    	wxsOK = false;
+        Dlg = new wimicDialog(0);
+        SetTopWindow(Dlg);
+        if (!wmsystem_status.silent_mode) {
+            Dlg->ShowModal();
+            wxsOK = false;
+        }
     }
     //*)
     return wxsOK;
 
+}
+
+int wimicApp::OnExit()
+{
+    return 0;
 }
--- wimicApp.h
+++ wimicApp.h
@@ -3,11 +3,21 @@
 #define WIMICAPP_H
 
 #include <wx/app.h>
+#include <wx/cmdline.h>
+#include <wx/msgdlg.h>
+
+class wimicDialog;
 
 class wimicApp : public wxApp
 {
     public:
         virtual bool OnInit();
+        virtual int OnExit();
+
+        wimicDialog *Dlg;
+
 };
+
+DECLARE_APP(wimicApp)
 
 #endif // WIMICAPP_H
