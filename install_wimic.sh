60b4ef62d0506d4b7de7603a6c8af367c64894e3
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2020-08-11 09:35:06 -0400

    install: added download wimic tarball from repo.

--- install_wimic.sh
+++ install_wimic.sh
@@ -3,6 +3,28 @@
 URUSSPATH=/system/urus/bin/
 export CLONE_URUSSTUDIO=0
 
+BITARCH=`uname -m`
+OSKERNEL=`uname -s | awk '{print tolower($0)}'`
+PLATOS=`cat /etc/os-release | grep -rwi - -e "ID" | cut -f2 -d=`
+URLDOWNLOAD=https://github.com/hiro2233/wimic/releases/download
+URLCHANGELOG=https://raw.githubusercontent.com/hiro2233/wimic/master/CHANGELOG
+
+if [ ! -n $PLATOS ] || [ "x$PLATOS" != "x" ] ; then
+TRIPLETNAME=$OSKERNEL-$PLATOS-$BITARCH
+echo $TRIPLETNAME
+else
+TRIPLETNAME=$OSKERNEL$BITARCH
+echo $TRIPLETNAME
+fi
+
+rm -f CHANGELOG.txt
+wget -O CHANGELOG.txt $URLCHANGELOG
+grep -ri CHANGELOG.txt -e "version pre-$TRIPLETNAME:" | awk '{print $3}' | cut -f2 -d: | head -n1 | xargs printf %s > version.txt
+wget -c $URLDOWNLOAD/$(cat version.txt)/wimic.tar.gz
+tar -xvzf wimic.tar.gz 2>/dev/null
+cp -rf wimic/* . 2>/dev/null
+rm -rf wimic 2>/dev/null
+
 ./install_deps.sh
 
 cp -f ./bin/Release/wimic $URUSSPATH
