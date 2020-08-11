c57f2361250a94eff2295528e287f979c77b3f31
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2020-08-11 06:29:38 -0400

    scripts: added check if wimic_mic_disabled.txt exist on slotdata.

--- scripts/wimic_service.sh
+++ scripts/wimic_service.sh
@@ -4,13 +4,18 @@ export PATH=/system/urus/bin:/bin:/sbin:/usr/bin:/usr/sbin:$PATH
 export DISPLAY=:0
 
 DEV=1
-MIC_DISABLED=`cat /system/urus/slotdata/wimic_mic_disabled.txt || true`
-
+MIC_DISABLED=`cat /system/urus/slotdata/wimic_mic_disabled.txt 2>/dev/null`
+#if [ "$MIC_DISABLED" = 1 ] || [ ! -n $MIC_DISABLED ] ; then
+#printf "disabled %d\n" "$MIC_DISABLED"
+#fi
+#exit 0
 DELAYSTART=10
 
 if [ "x$SSH_CLIENT" = "x" ] || [ "x$SSH_TTY" = "x" ] ; then
 printf "Starting WiMic...\n"
-	if [ $MIC_DISABLED -eq 0 ] ; then
+	if [ "$MIC_DISABLED" = 1 ] || [ ! -n $MIC_DISABLED ] ; then
+		sudo rmmod snd-aloop 2>/dev/null
+	else
 		DEV=5
 		sudo modprobe snd-aloop
 	fi
